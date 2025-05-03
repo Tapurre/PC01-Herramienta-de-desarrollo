7cbc6ad438271b834fd321cb8363967d51cbcc46
Luis Tacure <u22219116@utp.edu.pe>
2025-05-02 19:24:43 -0500

    SpringBoot

---
+++ soporte/src/test/java/PC01_Herramienta/de/desarrollo/soporte/SoporteApplicationTests.java
@@ -0,0 +1,13 @@
+package PC01_Herramienta.de.desarrollo.soporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
